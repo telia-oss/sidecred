4d391d7e74110348718f8da425dcfa7f1ddadeed
Måns Norelius <mans.norelius@teliacompany.com>
2022-11-29 17:56:18 +0100

    Add multiple github client retry

--- store/github/app.go
+++ store/github/app.go
@@ -1,6 +1,10 @@
 package github
 
-import "github.com/telia-oss/githubapp"
+import (
+	"fmt"
+
+	"github.com/telia-oss/githubapp"
+)
 
 // App is the interface that needs to be satisfied by the Github App implementation.
 //
@@ -10,4 +14,20 @@ type App interface {
 		*githubapp.Token,
 		error,
 	)
+}
+
+type MultiApp []App
+
+func (apps MultiApp) CreateInstallationToken(owner string, repositories []string, permissions *githubapp.Permissions) (*githubapp.Token, error) {
+	var err error
+	for _, app := range apps {
+
+		token, err := app.CreateInstallationToken(owner, repositories, permissions)
+		if err == nil {
+			fmt.Printf("created installation token, owner = %v\n", owner)
+			return token, nil
+		}
+	}
+
+	return nil, fmt.Errorf("create secrets access token: %w", err)
 }
