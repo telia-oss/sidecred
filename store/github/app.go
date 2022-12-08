ce56728faf77398085ba47310741d2b56c74bcb1
lostvicking <lostvicking@gmail.com>
2022-12-08 10:21:50 +0100

    adds rate limits check

--- store/github/app.go
+++ store/github/app.go
@@ -3,6 +3,7 @@ package github
 import (
 	"fmt"
 
+	"github.com/google/go-github/v45/github"
 	"github.com/telia-oss/githubapp"
 )
 
@@ -14,6 +15,7 @@ type App interface {
 		*githubapp.Token,
 		error,
 	)
+	RateLimits() (*github.RateLimits, *github.Response, error)
 }
 
 type MultiApp []App
@@ -21,13 +23,28 @@ type MultiApp []App
 func (apps MultiApp) CreateInstallationToken(owner string, repositories []string, permissions *githubapp.Permissions) (*githubapp.Token, error) {
 	var err error
 	for _, app := range apps {
-
+		// adds call to RateLimits here and continues to the next app if remaining is 0
+		rateLimits, _, err := app.RateLimits()
+		switch {
+		case err != nil:
+			fmt.Printf("rate limits error: %s", err)
+			continue
+		case rateLimits == nil || rateLimits.Core == nil:
+			fmt.Printf("rateLimits or rateLimits.Core is nil")
+			continue
+		case rateLimits.Core.Remaining == 0:
+			fmt.Printf("rateLimits.Core.Remaining is 0")
+			continue
+		}
 		token, err := app.CreateInstallationToken(owner, repositories, permissions)
 		if err == nil {
-			fmt.Printf("created installation token, owner = %v\n", owner)
 			return token, nil
 		}
 	}
 
 	return nil, fmt.Errorf("create secrets access token: %w", err)
+}
+
+func (apps MultiApp) RateLimits() (*github.RateLimits, *github.Response, error) {
+	return nil, nil, nil
 }
